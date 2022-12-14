0f4ff5a867e3384efe109545df7f397b33c98833
Gustav Öhman <se-gusohma-01@SE-DEVPC1079>
2022-12-14 09:38:17 +0100

    Day 14

--- Day14/Solution.fsx
+++ Day14/Solution.fsx
@@ -26,8 +26,12 @@ let setOfCoords (coords: (int*int) seq) =
     |> Seq.fold 
         (fun (set,(lastx, lasty)) (x,y) ->
             let line =
-                if lastx = x then {lasty..y} |> Seq.map (fun y -> (x,y)) //math abs!
-                elif lasty = y then {lastx..x} |> Seq.map (fun x -> (x,y))
+                if lastx = x then
+                    (if lasty <= y then {lasty..y} else {y..lasty}) 
+                    |> Seq.map (fun y -> (x,y))
+                elif lasty = y then 
+                    (if lastx <= x then {lastx..x} else {x..lastx}) 
+                    |> Seq.map (fun x -> (x,y))
                 else failwith "not a straight line"
                 |> Set.ofSeq
             (set |> Set.union line, (x,y)) 
@@ -47,16 +51,43 @@ let parseInput (input: string seq) =
 let willFallOff (map: (int*int) Set) (x,y) =
     map |> Set.exists (fun (x',y') -> x' = x && y' > y) |> not
 
-let rec restingPos (map: (int*int) Set) (x,y) =
+let rec restingPos1 (map: (int*int) Set) (x,y) =
     if willFallOff map (x,y) then None
-    elif not Set.contains (x, (y + 1)) map 
-        && Set.contains ((x), (y + 1)) map
+    elif not (Set.contains (x, (y + 1)) map)   then restingPos1 map (x, (y + 1))
+    elif not (Set.contains (x-1, (y + 1)) map) then restingPos1 map (x-1, (y + 1))
+    elif not (Set.contains (x+1, (y + 1)) map) then restingPos1 map (x+1, (y + 1))
+    else Some (x,y)
+
+let floor (map: (int*int) Set) =
+    (map |> Set.toSeq |> Seq.map snd |> Seq.max) + 2
+
+let rec restingPos2 floor (map: (int*int) Set) (x,y) =
+    if Set.contains (x,y) map then None
+    elif y = floor - 1 then Some (x,y)
+    elif not (Set.contains (x, (y + 1)) map)   then restingPos2 floor map (x, (y + 1))
+    elif not (Set.contains (x-1, (y + 1)) map) then restingPos2 floor map (x-1, (y + 1))
+    elif not (Set.contains (x+1, (y + 1)) map) then restingPos2 floor map (x+1, (y + 1))
+    else Some (x,y)
+
+let rec pourGrain restFun (x,y) (map: (int*int) Set) =
+    match restFun map (x,y) with
+    | Some (x',y') -> map |> Set.add (x',y') |> pourGrain restFun (x,y)
+    | None -> map
+
 let part1 input =
     let map = parseInput input
-
+    let filledMap = map |> pourGrain restingPos1 (500,0)
+    Console.WriteLine $"{Set.count filledMap} - {Set.count map} "
+    (Set.count filledMap) - (Set.count map)
+    |> sprintf "%i grains"
 
 let part2 input = 
-    Ok "todo"
+    let map = parseInput input
+    let floor = floor map
+    let filledMap = map |> pourGrain (restingPos2 floor) (500,0)
+    Console.WriteLine $"{Set.count filledMap} - {Set.count map} "
+    (Set.count filledMap) - (Set.count map)
+    |> sprintf "%i grains"
 
 module Tests =
     let private tests = 
@@ -74,8 +105,8 @@ match input with
     Result.map3
         (sprintf "Successful run!\r\nTests: %s\r\nPart1: %s\r\nPart2: %s")
         (Tests.run ())
-        (part1 input)
-        (part2 input)
+        (input |>> part1)
+        (input |>> part2)
 | None ->
     Tests.run () |>> sprintf "Tests only:\r\n %s"
 |> function
