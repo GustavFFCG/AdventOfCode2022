e8eb025275dff3860a71ca3d1f00ca01bce5d426
GustavFFCG <gustav.ohman@ffcg.se>
2022-12-27 11:28:22 +0100

    Day 21 solved

--- Day21/Solution.fsx
+++ Day21/Solution.fsx
@@ -24,6 +24,7 @@ let readFile fileName =
 type Shout =
     | Value of int
     | Operation of {|Var1:string; Var2:string; Operator: char|}
+    | Equality of string
 
 let parseInput (strings: string seq) = 
     strings
@@ -46,18 +47,65 @@ let toCode (apor: Map<string, Shout>) =
         | Value i -> [$"let {apa} = {i}L"]
         | Operation op ->
             code op.Var1 @ code op.Var2 @ [$"let {apa} = {op.Var1} {op.Operator} {op.Var2}"]
+        | Equality _ -> failwith "Not handled in part 1"
     "open System"::code "root" @ ["Console.WriteLine $\"Answer {root}\""]
 
-let humnDependencies (apor: Map<string, Shout>) =
-    let rec go acc apa =
-        match apor[apa] with
-        | Value _i -> Set.empty
-        | Operation op when op.Var1 = "humn" || op.Var2 = "humn" ->
-            Set.add apa acc
-        | Operation op ->
-            acc |> Set.union (go acc op.Var1) |> Set.union (go acc op.Var2)
+let toCode2 (apor: Map<string, Shout>) =
 
-    go Set.empty "root"
+    let reverseOp x switch y op z  =
+        //Console.WriteLine $"Reversing {x} = {y} {op} {z} on {switch}"
+        if x = "root" then
+            switch, Equality (if switch = y then z else y)
+        else
+            let var1, op , var2 =
+                match op with 
+                | '+' -> if switch = y then x, '-', z else x, '-', y
+                | '-' -> if switch = y then z, '+', x else y, '-', x
+                | '*' -> if switch = y then x, '/', z else x, '/', y
+                | '/' -> if switch = y then z, '*', x else y, '/', x
+                | c -> failwith $"unexpected operator {c}"
+            switch, Operation {| Var1 = var1 ; Operator = op; Var2 = var2 |}
+
+    let rec code reverse apa =
+        let (var, shout), reversed =
+            if reverse then
+                apor 
+                |> Map.findKey 
+                    (fun _key (s: Shout) -> 
+                        match s with | Operation op when op.Var1 = apa || op.Var2 = apa -> true | _ -> false
+                    )
+                |> (fun key ->
+                    apor[key]
+                    |> function 
+                    | Operation op ->
+                        if op.Var1 = apa then reverseOp key op.Var1 op.Var1 op.Operator op.Var2, Some key
+                        else reverseOp key op.Var2 op.Var1 op.Operator op.Var2, Some key
+                    | _ -> failwith "cannot reverse value or equality" 
+                )
+            else
+                (apa, apor[apa]), None
+        match shout with
+        | Value i -> [$"let {var} = {i}L"]
+        | Equality eq ->
+            code false eq 
+            @ [$"let {var} = {eq}"]
+        | Operation op ->
+            (if reverse then
+                if reversed = Some op.Var1 then
+                    code true op.Var1
+                    @ code false op.Var2 
+                else
+                    code true op.Var2
+                    @ code false op.Var1
+            else
+                code false op.Var1
+                @ code false op.Var2 
+            )
+            //code false op.Var1
+            //@ code reverse op.Var2 
+            @ [$"let {var} = {op.Var1} {op.Operator} {op.Var2}"]
+    
+    "open System"::(code true "humn") @ ["Console.WriteLine $\"Answer {humn}\""]
 
 let part1 input =
     parseInput input
@@ -66,15 +114,9 @@ let part1 input =
     "Written"
 
 let part2 input =
-    let apor = parseInput input
-    let humnDependencies = humnDependencies apor
-    let root = 
-        apor["root"]
-        |> function
-            | Operation op when Set.contains op.Var2 humnDependencies ->
-                {| Var1 = op.Var2; Var2 = op.Var1; Operator= op.Operator |} |> Operation
-            | Operation op -> Operation op
-            | Value _ -> failwith "Root was value"
+    parseInput input
+    |> toCode2
+    |> fun  code -> File.WriteAllLines("part2.fsx", code)
     "Written"
 
 module Tests =
